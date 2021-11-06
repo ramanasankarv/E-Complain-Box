002af39168ad64c48175e1bc472bc3922382d76c
kapil <kapildevsharma.cs@gmail.com>
2021-11-06 11:01:40 +0530

    added fav cion

--- src/components/Aboutus/Aboutus.jsx
+++ src/components/Aboutus/Aboutus.jsx
@@ -1,10 +1,18 @@
 import { Grid, Typography, Box } from '@mui/material';
 import React from 'react';
+import PanelHeader from '../../Shared/common/PanelHeader';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import Container from '@mui/material/Container';
 function Aboutus(props) {
     return (
-<Grid container>
-hi
-</Grid>
+<Container fixed py={2}>
+<Grid item container py={2} style={{ backgroundColor: "#2B7A78" }}>
+                <PanelHeader title={"About Us "} />
+            </Grid>
+        <Box sx={{ bgcolor: '#ffffff', height: '10vh' }} />
+        
+      </Container>
 
 
     );
